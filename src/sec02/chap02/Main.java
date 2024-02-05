560a7b08ad1610ea5b1bae26483032c7aabd5f44
ChangMinPark2 <98qkrckdals@naver.com>
2024-02-05 17:08:07 +0900

    feat(java): 자료형과 변수 ex1

--- src/sec02/chap02/Main.java
+++ src/sec02/chap02/Main.java
@@ -1,6 +1,6 @@
-package sec02;
+package sec02.chap02;
 
-public class Chap02 {
+public class Main {
     public static void main(String[] args) {
         //안녕을 출력해 주는 문이다.
         System.out.println("hi");
