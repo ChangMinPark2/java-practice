7ce0de97be9ca9f0e13fe8b5d008256c6f907507
ChangMinPark2 <98qkrckdals@naver.com>
2024-02-19 18:21:39 +0900

    feat(java): try with resources

---
+++ src/sec10/chap05/Ex02.java
@@ -0,0 +1,21 @@
+package sec10.chap05;
+
+public class Ex02 {
+    public static void dirtyOperation () {
+        try (SuicideSquad sc = new SuicideSquad()) {
+            sc.doSecretTask();
+        } catch (OpFailException e) {
+            //  💡 예외상황은 아만다 윌러가 책임짐
+            e.printStackTrace();
+            System.out.println("🗑️ 증거 인멸\n- - - - -\n");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            dirtyOperation();
+        }
+    }
+}
