d7a6cb7c8facff1c0602a373efa8e60d00866d83
ChangMinPark2 <98qkrckdals@naver.com>
2024-02-19 12:20:38 +0900

    feat(java): Comparable & Comparator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        list.add(1);
+
     }
 }
