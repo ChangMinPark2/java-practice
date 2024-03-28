a3437660b1aa1a3dd392edf282a1f060fbfa1761
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-28 11:20:13 +0900

    feat(java): test

--- src/testtest/UserCode.java
+++ src/testtest/UserCode.java
@@ -3,22 +3,23 @@ package testtest;
 public class UserCode {
 	public String code =
 		"import java.io.*;\n" +
-		"public class Main {\n" +
-		"    public static void main(String[] args) {\n" +
-		"        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n" +
-		"        int a, b;\n" +
-		"        try {\n" +
-		"            a = Integer.parseInt(br.readLine());\n" +
-		"            b = Integer.parseInt(br.readLine());\n" +
-		// "        while(true) {\n" +
-		// "        }\n" +
-		"            // 계산하고 결과 출력\n" +
-		"            System.out.println(a + b + 1);\n" +
-		"        } catch (IOException e) {\n" +
-		"            e.printStackTrace();\n" +
-		"        }\n" +
-		"    }\n" +
-		"}\n";
+			"public class Main {\n" +
+			"    public static void main(String[] args) {\n" +
+			"        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n" +
+			"        int a, b;\n" +
+			"        try {\n" +
+			"            a = Integer.parseInt(br.readLine());\n" +
+			"            b = Integer.parseInt(br.readLine());\n" +
+			"            // 임의로 시간 지연을 추가하여 시간 초과를 유발\n" +
+			"            Thread.sleep(500);\n" + // 5초 동안 일시 정지
+			"            System.out.println(a + b);\n" +
+			"        } catch (IOException e) {\n" +
+			"            e.printStackTrace();\n" +
+			"        } catch (InterruptedException e) {\n" +
+			"            e.printStackTrace();\n" +
+			"        }\n" +
+			"    }\n" +
+			"}\n";
 
 	public String getCode() {
 		return code;
